651ff57d6c3466e17274fb9977eee97772321846
cesurkedi <cesurkedixx@protonmail.com>
2023-04-06 22:06:38 +0300

    side menu finished

---
+++ TwitterClone/Core/Components/Tweets/TweetRowView.swift
@@ -0,0 +1,89 @@
+//
+//  TweetRowView.swift
+//  TwitterClone
+//
+//  Created by Çare C. on 2.04.2023.
+//
+
+import SwiftUI
+
+struct TweetRowView: View {
+    var body: some View {
+        VStack (alignment: .leading) {
+            //Profile image + user info + tweet
+            HStack(alignment: .top, spacing: 12){
+                Circle()
+                    .frame(width: 56, height: 56)
+                    .foregroundColor(Color(.systemBlue))
+                
+                // User info & tweet caption
+                VStack (alignment: .leading, spacing: 4) {
+                    // User info
+                    HStack {
+                        Text("Bruce Wayne")
+                            .font(.subheadline).bold()
+                        
+                        Text("@batman")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                        
+                        Text("2w")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                    }
+                    
+                    //text caption
+                    Text("I believe in harvey dent")
+                        .font(.subheadline)
+                        .multilineTextAlignment(.leading)
+                }
+            }
+            // action buttons
+            HStack {
+                Button {
+                    //action goes here ...
+                } label: {
+                    Image(systemName: "bubble.left")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button {
+                    //action goes here ...
+                } label: {
+                    Image(systemName: "arrow.2.squarepath")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button {
+                    //action goes here ...
+                } label: {
+                    Image(systemName: "heart")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button {
+                    //action goes here ...
+                } label: {
+                    Image(systemName: "bookmark")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            .foregroundColor(.gray)
+            
+            Divider()
+        }
+    }
+}
+
+struct TweetRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetRowView()
+    }
+}
